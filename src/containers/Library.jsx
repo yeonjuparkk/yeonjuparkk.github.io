0b828f12d313b1f636eca5c4e4b8bd1309b14df5
Yeonju Park <yeonjupark95@gmail.com>
2022-12-16 15:15:29 -0600

    remove article component

--- src/containers/Library.jsx
+++ src/containers/Library.jsx
@@ -1,5 +1,5 @@
 import "./Library.css";
-import { Article, CTA } from "../components";
+import { CTA } from "../components";
 import {
   wedding1,
   wedding2,
@@ -16,26 +16,6 @@ const Library = () => {
     <div className="portfolio__library section__padding" id="photography">
       <div className="portfolio__library-heading">
         <h1 style={{ color: "white" }}>Photography</h1>
-      </div>
-      <div className="portfolio__library-container">
-        <div className="portfolio__library-container_groupA">
-          <Article imgUrl={wedding1} />
-        </div>
-        <div className="portfolio__library-container_groupB">
-          <Article imgUrl={wedding2} />
-        </div>
-        <div className="portfolio__library-container_groupA">
-          <Article imgUrl={wedding3} />
-        </div>
-        <div className="portfolio__library-container_groupA">
-          <Article imgUrl={wedding4} />
-        </div>
-        <div className="portfolio__library-container_groupA">
-          <Article imgUrl={weddingDetail1} />
-        </div>
-        <div className="portfolio__library-container_groupA">
-          <Article imgUrl={wedding5} />
-        </div>
       </div>
       <CTA />
     </div>
