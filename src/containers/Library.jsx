10dfb5f3f5e66a71531d997fad811ba4ba376ee2
Yeonju Park <yeonjupark95@gmail.com>
2022-12-17 16:59:47 -0600

    add slideshow for photography

--- src/containers/Library.jsx
+++ src/containers/Library.jsx
@@ -1,4 +1,6 @@
 import "./Library.css";
+import { Slide } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
 import { CTA } from "../components";
 import {
   wedding1,
@@ -11,12 +13,59 @@ import {
   weddingDetail3,
 } from "../assets";
 
+const images = [
+  `${wedding1}`,
+  `${weddingDetail1}`,
+  `${weddingDetail2}`,
+  `${wedding3}`,
+  `${wedding2}`,
+  `${wedding4}`,
+  `${wedding5}`,
+];
+
 const Library = () => {
   return (
     <div className="portfolio__library section__padding" id="photography">
       <div className="portfolio__library-heading">
         <h1 style={{ color: "white" }}>Photography</h1>
       </div>
+      <Slide>
+        <div className="portfolio__library-each-slide-effect">
+          <div>
+            <img src={`${images[0]}`} alt="photography" />
+          </div>
+        </div>
+        <div className="portfolio__library-each-slide-effect">
+          <div>
+            <img src={`${images[1]}`} alt="photography" />
+          </div>
+        </div>
+        <div className="portfolio__library-each-slide-effect">
+          <div>
+            <img src={`${images[2]}`} alt="photography" />
+          </div>
+        </div>
+        <div className="portfolio__library-each-slide-effect">
+          <div>
+            <img src={`${images[3]}`} alt="photography" />
+          </div>
+        </div>
+        <div className="portfolio__library-each-slide-effect">
+          <div>
+            <img src={`${images[4]}`} alt="photography" />
+          </div>
+        </div>
+        <div className="portfolio__library-each-slide-effect">
+          <div>
+            <img src={`${images[5]}`} alt="photography" />
+          </div>
+        </div>
+        <div className="portfolio__library-each-slide-effect">
+          <div>
+            <img src={`${images[6]}`} alt="photography" />
+          </div>
+        </div>
+      </Slide>
       <CTA />
     </div>
   );
