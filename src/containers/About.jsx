fb5f0e4390d3bdcee94d507139607dcb209eed7c
Yeonju Park <yeonjupark95@gmail.com>
2023-01-03 23:47:45 -0600

    change about section to framer motion

--- src/containers/About.jsx
+++ src/containers/About.jsx
@@ -1,9 +1,18 @@
 import "./About.css";
 import { instaStory1, instaStory2, instaStory3, instaStory4 } from "../assets";
-import { Slide } from "react-slideshow-image";
+import { useState, useEffect } from "react";
+import { motion as m } from "framer-motion";
 
 const About = () => {
-  const images = [`${instaStory1}`, `${instaStory2}`, `${instaStory3}`, `${instaStory4}`];
+  const images = [instaStory1, instaStory2, instaStory3, instaStory4];
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex(index + 1);
+    }, 4000);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div
@@ -13,29 +22,13 @@ const About = () => {
       <div className="portfolio__about-heading">
         <h1 className="gradient__text">About</h1>
       </div>
-      <div>
-        <Slide>
-          <div className="portfolio__library-each-slide-effect">
-            <div>
-              <img src={`${images[0]}`} alt="photography" />
-            </div>
-          </div>
-          <div className="portfolio__library-each-slide-effect">
-            <div>
-              <img src={`${images[1]}`} alt="photography" />
-            </div>
-          </div>
-          <div className="portfolio__library-each-slide-effect">
-            <div>
-              <img src={`${images[2]}`} alt="photography" />
-            </div>
-          </div>
-          <div className="portfolio__library-each-slide-effect">
-            <div>
-              <img src={`${images[3]}`} alt="photography" />
-            </div>
-          </div>
-        </Slide>
+      <div className="portfolio__about-content">
+        <m.img
+          src={images[index]}
+          initial={{ opacity: 0 }}
+          transition={{ duration: 4 }}
+          animate={{ opacity: 1 }}
+        />
       </div>
     </div>
   );
