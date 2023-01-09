9260c05b01c189bd8b0bc4aa640dcab36f887f08
Yeonju Park <yeonjupark95@gmail.com>
2023-01-08 23:31:44 -0600

    Updates

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Social } from "./Social";
-export { default as Contact } from "./Contact";
-export { default as Project } from "./Project";
-export { default as Navbar } from "./Navbar";
-export { default as Skills } from "./Skills";
-export { default as Timeline } from "./Timeline";
--- src/containers/index.js
+++
@@ -1,6 +0,0 @@
-export { default as About } from "./About";
-export { default as Experience } from "./Experience";
-export { default as Library } from "./Library";
-export { default as Projects } from "./Projects";
-export { default as Footer } from "./Footer";
-export { default as Header } from "./Header";;
